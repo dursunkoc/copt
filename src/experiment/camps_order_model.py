bcf992895cc7510d23d21037d85214c5ad90775e
dursunkoc <dursunkoc@gmail.com>
2021-05-23 09:14:00 +0300

    improving camps order

--- src/experiment/camps_order_model.py
+++ src/experiment/camps_order_model.py
@@ -1,11 +1,9 @@
 from docplex.mp.model import Model
+from experiment import Solution,  SolutionResult, Case, Experiment, Parameters
 import numpy as np
 
 def start_model(C, D, I, l_c, q_ic, m_i, n_i):
     mdl = Model(name='Campaigns Order Optimization')
-    Y = mdl.continuous_var_dict((c,d)
-                for c in range(0,C)
-                for d in range(0,D))
     Y = {(c,d): mdl.continuous_var(lb=0, name=f"Y_c:{c}_d:{d}")
             for c in range(0,C)
             for d in range(0,D)}
@@ -31,25 +29,37 @@ def start_model(C, D, I, l_c, q_ic, m_i, n_i):
     return mdl, Y
 
 if __name__ == '__main__':
-    C=5
-    D=7
-    I=3
-    P=3
-    r_p = np.random.choice(100, P) #r_p = np.ones(P, dtype='int8')
-    rp_c = np.array([r_p[r] for r in np.random.choice(P, C)])
-    q_ic = np.random.choice(2, (I,C)) #q_ic = np.zeros((I,C), dtype='int8')
-    l_c = np.random.choice([2,3,4],C)
-    ##quota limitations daily/weekly
-    m_i = np.random.choice([4,3,5],I)#m_i = np.ones((I), dtype='int8')*10
-    n_i = np.random.choice([1,3,2],I)#n_i = np.ones((I), dtype='int8')*10
+    case = Case({"C":5,"U":200,"H":3, "D":7, "I":3, "P":3})#3
+    solution = Solution("Many")
+    PMS:Parameters = solution.generate_parameters(case, None)
+    C = case.arguments["C"] # number of campaigns
+    U = case.arguments["U"]  # number of customers.
+    H = case.arguments["H"]  # number of channels.
+    D = case.arguments["D"]  # number of planning days.
+    I = case.arguments["I"]  # number of planning days.
 
-    mdl, Y = start_model(C, D, I, l_c, q_ic, m_i, n_i)
+    mdl, Y = start_model(C, D, I, PMS.l_c, PMS.q_ic, PMS.m_i, PMS.n_i)
     #print(mdl.objective_expr)
     #for consts in mdl.iter_constraints():
     #    print(consts)
+    print("q_ic:",PMS.q_ic)
+    print("m_i:",PMS.m_i)
+    print("n_i:",PMS.n_i)
+    print("l_c:",PMS.l_c)
     result = mdl.solve()
     print(f"OV:{result.objective_value}")
-    for d in range(D):
-        y_vl = [(int(result.get_var_value(Y[(c,d)])*1000)+l_c[c]) for c in range(C)]
-        print(f"{d}->{y_vl}")
-        print(f"\t==> rp_c* -->{rp_c*y_vl}")
+    print(
+        np.array(
+            [
+                [vv[0] if vv[0]>0 else vv[1] for vv in [(result.get_var_value(Y[(c,d)])*10000, PMS.l_c[c]) for c in range(C)] ]
+            for d in range(D)]
+            , dtype='int').min(axis=0) * PMS.rp_c
+        )
+
+    print(
+        np.array(
+            [
+                [(result.get_var_value(Y[(c,d)])) for c in range(C)]
+            for d in range(D)]
+            , dtype='int')
+        )
