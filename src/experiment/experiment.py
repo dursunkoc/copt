96e2a3c7dca8e27779a1f2bffdac9e5eb328c38a
TURKCELL\TTDKOC <dursun.koc@turkcell.com.tr>
2023-04-02 19:23:12 +0300

    Completed with samples

--- src/experiment/experiment.py
+++ src/experiment/experiment.py
@@ -166,6 +166,7 @@ class Solution:
         m_i = np.random.choice([4,3,5],I)#m_i = np.ones((I), dtype='int8')*10
         n_i = np.random.choice([1,3,2],I)#n_i = np.ones((I), dtype='int8')*10
         ##capacity for channel
+#        t_hd = np.random.choice([U*.7, U*.6, U*.5], (H, D))
         t_hd = np.random.choice([U*.01, U*.02, U*.03], (H, D))
         e_cu_X = np.stack([np.stack([e_cu for _ in range(H)], axis=2) for _ in range(D)], axis=3)
         m_i_X = np.stack([m_i for _ in range(U)], axis=1)
